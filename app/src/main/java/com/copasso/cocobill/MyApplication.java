7a1142a46296f37958308421b55294c39c654e6b
huangming <huangming@bdstar.com>
2021-04-16 09:33:46 +0800

    init

---
+++ app/src/main/java/com/copasso/cocobill/MyApplication.java
@@ -0,0 +1,51 @@
+package com.copasso.cocobill;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.copasso.cocobill.model.bean.remote.MyUser;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobUser;
+
+/**
+ * Created by Zhouas666 on AndroidStudio
+ * Date: 2019-01-08
+ * Github: https://github.com/zas023
+ */
+public class MyApplication extends Application {
+
+    public static MyApplication application;
+    private static Context context;
+    private static MyUser currentUser;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        application = this;
+        context = getApplicationContext();
+        //初始化Bmob后端云
+        Bmob.initialize(this, "c633813fcf88bf94058100c3ff1d3f75");
+        currentUser = BmobUser.getCurrentUser(MyUser.class);
+    }
+
+    /**
+     * 获取上下文
+     *
+     * @return
+     */
+    public static Context getContext() {
+        return context;
+    }
+
+    /**
+     * 获取用户id
+     * @return
+     */
+    public static String getCurrentUserId() {
+        currentUser = BmobUser.getCurrentUser(MyUser.class);
+        if (currentUser == null)
+            return null;
+        return BmobUser.getCurrentUser(MyUser.class).getObjectId();
+    }
+}
